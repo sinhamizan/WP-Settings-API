084c244740f23e9bb19ec69582b24c11343d644d
Mizanur Rahaman <mizan75kpi@gmail.com>
2021-06-12 17:45:18 +0600

    Create any-file.php

---
+++ any-file.php
@@ -0,0 +1,4 @@
+<a href="<?php echo get_option( 'custom_fb_field' ); ?>">Facebook</a>
+<a href="<?php echo get_option( 'custom_yt_field' ); ?>">Youtube</a>
+<a href="<?php echo get_option( 'custom_lin_field' ); ?>">LinkedIn</a>
+<a href="<?php echo get_option( 'custom_twt_field' ); ?>">Twitter</a>
